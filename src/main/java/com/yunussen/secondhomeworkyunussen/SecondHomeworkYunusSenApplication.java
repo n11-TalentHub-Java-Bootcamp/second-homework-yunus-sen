966725749e5c496bbb487ae2c5196b7a16ab692e
YUNUS <yunussen@ycp.com.tr>
2021-12-13 12:33:26 +0300

    project was created

---
+++ src/main/java/com/yunussen/secondhomeworkyunussen/SecondHomeworkYunusSenApplication.java
@@ -0,0 +1,13 @@
+package com.yunussen.secondhomeworkyunussen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHomeworkYunusSenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHomeworkYunusSenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunussen/secondhomeworkyunussen/SecondHomeworkYunusSenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunussen.secondhomeworkyunussen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHomeworkYunusSenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
